6770c0670c813903a57ebe9f88fceac0da6f10cd
deepagurung11 <deepagurung400@gmail.com>
2024-05-26 00:02:40 +0600

    hw6

---
+++ hw6.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the number for which you want the multiplication table: "))
+limit = int(input("Enter the limit up to which you want the table generated: "))
+
+print(f"Multiplication Table for {number}:")
+for i in range(1, limit + 1):
+    print(f"{number} x {i} = {number * i}")
